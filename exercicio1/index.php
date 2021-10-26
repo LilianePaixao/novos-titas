799104a8c0421cdb4a3857cc99cd33d3f71695ad
Liliane Paixão <cpaixaoliliane@gmail.com>
2021-10-26 09:52:36 -0300

    Move exercise1 for folder exercise 1

---
+++ exercicio1/index.php
@@ -0,0 +1,22 @@
+<html>
+</html>
+
+<?php
+/**
+ *  Exercise 1
+ */
+$value1 = $argv[1];
+$value2 = $argv[2];
+
+$sum = $value1 + $value2;
+
+if($sum > 20){
+    //sum = $sum +8;
+    $sum += 8;
+ 
+} else
+    $sum -= 5;
+
+echo 'O resultado da soma é ' . $sum."\n";
+
+
