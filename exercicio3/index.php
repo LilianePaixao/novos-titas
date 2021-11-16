dc41f557cfa52ce91d57837f164044d487171c3b
Liliane Paixão <cpaixaoliliane@gmail.com>
2021-11-16 14:12:54 -0300

    Executed

---
+++ exercicio3/index.php
@@ -0,0 +1,18 @@
+<html>
+    <?php
+/* Entrar com nome, sexo e idade de uma
+pessoa. Se a pessoa for do sexo feminino e
+tiver menos que 25 anos, imprimir nome e a
+a mensagem: ACEITA. Caso contrário,
+imprimir nome e a mensagem: NÃO ACEITA.*/
+        $nome = $_POST["nome"];
+        $sexo = $_POST["sexo"];
+        $idade = $_POST["idade"];
+            if ($_POST["fem"] && $_POST["idade"] < 25){ 
+            echo "$nome ACEITA";
+            }else{
+        echo "$nome NÃO ACEITA";
+            } 
+    ?>
+        <a href = "exercicio3.html">voltar</a>
+</html>
