4f2668193b33cc7795edaf7b36339199ce290951
Liliane Paixão <cpaixaoliliane@gmail.com>
2021-11-25 17:00:14 -0300

    Criaçao html e php exercicio 9

---
+++ exercicio9/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Exercicio9</title>
+</head>
+<body>
+    <div>
+        <form action = "index.php" method="POST">
+            
+            <label for="numero"> Digite um número:</label>
+            <input type ="number" name ="numero" id="numero">
+
+        </form>
+        <?php
+        /* Entrar com o número de vezes que se deseja
+imprimir a palavra SOL e imprimir */
+            for ($i=1;$i <= $_POST["numero"];$i++){
+                echo $i."SOL".PHP_EOL;
+            } 
+           
+               
+        ?>
+    </div>
+</body>
+</html>
