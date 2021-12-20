86cd1b9f24e9e138b5ecbc68e5595b632f43877c
LilianePaixao <cpaixaoliliane@gmail.com>
2021-12-20 11:06:06 -0300

    criaçao de um indice com ajuda do Arthur

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Índice</title>
+</head>
+
+<body>
+    <h1>Índice</h1>
+    <a href=/exercicio1>Exercício 1</a><br/>
+    <a href=/exercicio2>Exercício 2</a><br/>
+    <a href=/exercicio3>Exercício 3</a><br/>
+    <a href=/exercicio4>Exercício 4</a><br/>
+    <a href=/exercicio5>Exercício 5</a><br/>
+    <a href=/exercicio6>Exercício 6</a><br/>
+    <a href=/exercicio7>Exercício 7</a><br/>
+    <a href=/exercicio8>Exercício 8</a><br/>
+    <a href=/exercicio9>Exercício 9</a><br/>
+    <a href=/exercicio10>Exercício 10</a><br/>
+    <a href=/exercicio11>Exercício 11</a><br/>
+    <a href=/exercicio12>Exercício 12</a><br/>
+  </body>
+</html>
+
