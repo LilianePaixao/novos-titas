386b9e9d70b0ee5071137618630291b1624732ad
LilianePaixao <cpaixaoliliane@gmail.com>
2021-12-26 22:32:00 -0300

    Included style and code better

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -1,22 +1,46 @@
 <html>
-<?php
-/**
- *  Exercise 1
- */
-$value1 = $argv[1];
-$value2 = $argv[2];
+    <head> 
+            <link rel="stylesheet" href="style.css"    
+            <meta charset="UTF-8"/>
+            <title>Exercício 1 </title>
+        </head>
 
-$sum = $value1 + $value2;
+    <body>
+       
+        <div>
+            <h1> Exercício 1 </h1>
 
-if($sum > 20){
-    //sum = $sum +8;
-    $sum += 8;
- } else
-    $sum -= 5;
-echo 'O resultado da soma é ' . $sum."\n";
+            <form method="post" action="index.php">
+                
+                    <label for="n1">Digite o 1º número: </label>
+                    <input type="number" id="n1">
+                          
+                    <label for="n2">Digite o 2º número: </label>
+                    <input type="number" id="n2" >
+                  
+                    <input type="submit" value="Enviar"/>
+                
+                    <img id= "img" src="iconezap.png">
+            </form>
+                <?php
+                /* Construir um algoritmo que leia 2 números e
+                efetue a adição. Caso o valor somado seja
+                maior que 20, este deverá ser apresentando
+                somando-se a ele mais 8; caso o valor
+                somado seja menor ou igual a 20, este deverá
+                ser apresentado subtraindo-se 5
+                */
+                
+                $sum = $POST["n1"] + $POST["n2"];
+                echo " A soma dos dois números é $sum";
 
-?>
-</form>
-</div>
-</body
+                 if($sum > 20){
+                    echo "$sum  += 8";
+                } elseif($um <=20){
+                    echo "$sum -= 5";
+                }    
+                ?>
+    
+        </div>
+    </body
 </html>
