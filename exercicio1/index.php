d88c1ed4fc206746caeb86c5fa32f3360e170a57
LilianePaixao <cpaixaoliliane@gmail.com>
2021-12-30 16:37:31 -0300

    Exercicio 1 corrigido e com css

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
     <head> 
-            
+        <html lang="pt-br">   
         <html lang="pt-br">
         <meta charset="UTF-8">
         <link rel="stylesheet" href="/exercicio1/style.css">    
-        
         <title>Exercício 1 </title>
     </head>
 
     <body>
-       
         <div>
             <h1> Exercício 1 </h1>
 
             <form method="POST" action="/exercicio1/index.php">
                 
-                    <label for="n1">Digite o 1º número: </label>
+                    <label for="n1"> Digite o 1º número: </label>
                     <input type="number" name="n1">
                           
                     <label for="n2">Digite o 2º número: </label>
@@ -23,7 +21,7 @@
                   
                     <input type="submit" value="Enviar"/>
                 
-                    <img id= "img" src="iconezap.png">
+                    <img id= "img" src="/exercicio1/iconezap.png">
             </form>
 
                 <?php
@@ -34,15 +32,19 @@
                 somado seja menor ou igual a 20, este deverá
                 ser apresentado subtraindo-se 5
                 */
-                               
+                
+                
                 $sum = $_POST["n1"] + $_POST["n2"];
-
-                if($sum > 20){
-                    echo $sum += 8;
-
-                } elseif($um <=20){
-                    echo $sum -= 5;
-                }    
+                
+                if (empty($_POST["n1"] and $_POST["n2"])){
+                    echo "Preencha os campos acima";
+                }elseif($sum > 20){
+                    echo $sum +=8;
+                }else{
+                    echo $sum -=5;
+                }
+                       
+                
                 ?>
     
         </div>
