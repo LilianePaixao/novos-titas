3cbaee30d650fe461e1403deed16b47c328db2da
LilianePaixao <cpaixaoliliane@gmail.com>
2022-01-03 15:42:52 -0300

    Correçao solicitada

--- exercicio3/index.php
+++ exercicio3/index.php
@@ -12,7 +12,7 @@ pessoa. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nom
         <div class ="alert"> 
             <form method="post" action="/exercicio3/index.php">
                 <label for="name">Nome:</label> 
-                <input type="text" id="name"/><br/>
+                <input type="text" id="name" name="name"/><br/>
                 <fieldset><legend>Sexo</legend>
                     <input type="radio" name="sexo" id="masc" value="homem"/>
                     <label for="masc"> Masculino </label><br/>
@@ -21,7 +21,7 @@ pessoa. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nom
                 </fieldset>
                 <br/>   
                 <label for="idade">Idade:</label>
-                <input type="number" id="idade" value="idade"/><br/>
+                <input type="number" id="idade" name="idade" value="idade"/><br/>
                 <br/>
                 <input type="submit" value="Enviar"/>
             </form>                        
@@ -33,9 +33,9 @@ pessoa. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nom
                 if($_POST["name"] == null && $_POST["sexo"]== null && $_POST["idade"]== null){
                     echo ""; 
                 }elseif($_POST["sexo"] === "mulher" && $_POST["idade"] < 25 ){
-                    echo $_POST["name"] . "ACEITA";
+                    echo $_POST["name"] . " ACEITA";
                 }else{ 
-                    echo $_POST["name"] . "NÃO ACEITA";
+                    echo $_POST["name"] . " NÃO ACEITA";
                 }
                 
             ?>
