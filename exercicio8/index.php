a3c3a17353b610e85d2285ee83f362990fb663c6
LilianePaixao <cpaixaoliliane@gmail.com>
2022-01-21 13:44:46 -0300

    Function included

--- exercicio8/index.php
+++ exercicio8/index.php
@@ -1,41 +1,38 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <link rel="stylesheet" href="/exercicio8/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Exercicio8</title>
 </head>
 <body>
+    <h1>Exercício 8</h1>
+    <p>Criar um algoritmo que leia um número e  imprima todos os número de 1 até ele e o seu produto.</p>
     <div>
         <form action = "/exercicio8/index.php" method="POST">
-            
             <label for="numero"> Digite um número:</label>
             <input type ="number" size= "100" maxlength ="100" name ="numero" id="numero">
-            
-            <input type="submit" value="Enviar">
+            <input type="submit" value="Enviar" class="Enviar">
         </form>
         <?php
-        /* Criar um algoritmos que leia um número e  imprima todos os número de 1 até ele e o seu produto. */
+            $param1 = $_POST["numero"]; // dentro do post tem que ficar o name do input
             
-            $numeroInformado = $_POST["numero"]; // dentro do post tem que ficar o name do input
+            if(!empty($param1)){
+                readNumber($param1);
+            }
 
-            for ($i=1;$i <= $numeroInformado;$i++){
+        function readNumber($param1){    
+            for ($i=1; $i <= $param1; $i++){
                 echo "$i <br/>";
             } 
+                for ($c = 0; $c <= $param1; $c++){
+                $resultado = $param1 * $c;
+                echo "$param1 x  $c  = $resultado <br/>";
+                }        
+            }
            
-            for ($c =0; $c <= $numeroInformado; $c++){
-                $resultado = $numeroInformado * $c;
-                ?>
-
-                 <div class= 'camporesultado'>
-                  <?= "$numeroInformado x  $c  =   $resultado " ?>
-                 </div> <br/> 
-            
-            <?php     
-            }     
-            
         ?>
     </div>
 </body>
