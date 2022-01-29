b88d8c77b85571304cddab49c81ac044d5a109e8
LilianePaixao <cpaixaoliliane@gmail.com>
2022-01-29 12:13:11 -0300

    Inclusao do exercicio11

--- exercicio11/index.php
+++ exercicio11/index.php
@@ -1,27 +1,31 @@
-<html>
+<!DOCTYPE>
+<html lang="pt-br">
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/exercicio11/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Exercicio11</title>
 </head>
 <body>
     <div>
-        <form action = "index.php" method="POST">
+        <h1>Exercício 11</h1>
+        <p>Criar um algoritmos que imprima a tabuada de um número</p>
+        <form action = "/exercicio11/index.php" method="POST">
             
             <label for="numero"> Informe um número</label>
-            <input type ="number" name ="numero" id="numero">
-            <input type ="submit" value = "Enviar">          
-
+            <input type ="number" name ="numero">
+            <input type ="submit" value = "enviar" class="enviar">          
 
         </form>
         <?php
-        $numero = $_POST["numero"];
-              for ($i=0;$i <= 10;$i++){
-                echo $i ." x " .$_POST["numero"]. "=". $i*$_POST["numero"]. "<br/>";
-            }                   
+            $numero = $_POST["numero"];
+                if (isset($_POST[numero])){
+                    for ($i=0;$i <= 10;$i++){
+                        $resultado = $i * $numero;
+                        echo " $numero  x  $i =  $resultado <br/>";
+                    } 
+                }                      
         ?>
     </div>
 </body>
