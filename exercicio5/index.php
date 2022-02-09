31c46f7d10b79a71fb561c7e9a208d5414323273
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-09 15:11:01 -0300

    Exercicios 5,6 e 7

--- exercicio5/index.php
+++ exercicio5/index.php
@@ -26,6 +26,8 @@
         <?php
         if($_POST["n1"] == null && $_POST["n2"] == null && $_POST["n3"] == null){
             echo "";
+        }elseif($_POST["n1"] <= 0 || $_POST["n2"] <= 0 || $_POST["n3"] <= 0){
+        echo "Os números informados devem ser superior a 0 (zero)";    
         }elseif($_POST["n1"] <> $_POST["n2"] && $_POST["n1"] <> $_POST["n3"]) {
             echo "Trata-se de um triângulo escaleno, porque possui todos os lados diferentes: ". $_POST["n3"] ." , ". $_POST["n2"] . " e " . $_POST["n1"];
         }elseif ($_POST["n1"] == $_POST["n3"] && $_POST["n1"] == $_POST["n2"]){
