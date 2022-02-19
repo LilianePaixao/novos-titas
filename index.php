17a4064e5842a5a8be57a9f458aa87034fb98658
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-19 10:52:11 -0300

    Resolução de problemas dos testes com a inclusão do índice

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Índice</title>
+</head>
+<body>
+    <h1>Índice</h1>
+    <a href=/exercicio1>Exercício 1</a><br/>
+    <a href=/exercicio2>Exercício 2</a><br/>
+    <a href=/exercicio3>Exercício 3</a><br/>
+    <a href=/exercicio4>Exercício 4</a><br/>
+    <a href=/exercicio5>Exercício 5</a><br/>
+    <a href=/exercicio6>Exercício 6</a><br/>
+    <a href=/exercicio7>Exercício 7</a><br/>
+    <a href=/exercicio8>Exercício 8</a><br/>
+    <a href=/exercicio9>Exercício 9</a><br/>
+    <a href=/exercicio10>Exercício 10</a><br/>
+    <a href=/exercicio11>Exercício 11</a><br/>
+    <a href=/exercicio12>Exercício 12</a><br/>
+    <a href=/exercicio13>Exercício 13</a><br/>
+    <a href=/exercicio14>Exercício 14</a><br/>
+    <a href=/exercicio15>Exercício 15</a><br/>
+    <a href=/exercicio16>Exercício 16</a><br/>
+    <a href=/exercicio17>Exercício 17</a><br/>
+    <a href=/exercicio18>Exercício 18</a><br/>
+    <a href=/exercicio19>Exercício 19</a><br/>
+    <a href=/exercicio20>Exercício 20</a><br/>
+  </body>
+</html>
