1a056402531f458f761a1d701729921ab37b3c30
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-22 10:40:18 -0300

    correçao exercicio13

--- tests/acceptance/Exercicio13Cest.php
+++ tests/acceptance/Exercicio13Cest.php
@@ -3,7 +3,7 @@
 class Exercicios13Cest
 {
     
-    public function triangleType(AcceptanceTester $I)
+    public function Numbers100until200(AcceptanceTester $I)
     {
         $I->amOnPage('/exercicio13');
         $I->see('Exercício 13');
@@ -12,11 +12,20 @@ class Exercicios13Cest
         $I->click("enviar");
         $I->see('Você informou o número 0, por isso a execução foi parada        
         ');
-        //diferents numbers
-        //Number 
+        //Numbers target 
         $I->fillField('numero', '1,2,3,100,150,200');
         $I->click("enviar");
         $I->see('A quantidade de números entre 100 e 200 é 3        
+        ');
+        //Numbers below target
+        $I->fillField('numero', '1,2,3');
+        $I->click("enviar");
+        $I->see('Você não informou nenhum número entre 100 e 200        
+        ');
+        //Numbers above target
+        $I->fillField('numero', '201,202,210,300');
+        $I->click("enviar");
+        $I->see('Você não informou nenhum número entre 100 e 200        
         ');
     }
 }
