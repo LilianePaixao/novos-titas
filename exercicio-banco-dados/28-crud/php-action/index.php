32b5fd5f2fb0e9cdd93c142fa939e15fc05e2045
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-23 13:44:33 -0300

    Estudos de Banco de dados

---
+++ exercicio-banco-dados/28-crud/php-action/index.php
@@ -0,0 +1,48 @@
+<?php
+//header
+include_once 'includes/header.php';
+?>
+
+<div class="row">
+    <div class="col s12 m6 push-m3 red">
+        <h3 class="ligth">Clientes</h3>
+        <table class="stripped">
+            <thead>
+                <tr>
+                    <th>Nome:</th>
+                    <th>Sobrenome:</th>
+                    <th>Email:</th>
+                    <th>Idade:</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Rodrigo</td>
+                    <td>Oliveira</td>
+                    <td>liliane@gmail.com</td>
+                    <td>20</td>
+                    <td><a href="" class="btn-floating orange"></a><i class="material-icons">edit</i></td>
+                    <td><a href="" class="btn-floating red"></a><i class="material-icons">delete</i></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <a href="" class="btn"> Adicionar cliente</a>
+    </div>
+</div>
+
+
+    </div>
+</div>
+
+
+
+
+
+<?php
+//footer
+include_once 'includes/footer.php';
+?>
+
+    
