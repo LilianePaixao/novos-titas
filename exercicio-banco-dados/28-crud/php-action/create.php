e270b898fe0ad520c816bcfe58f49543fc8dc646
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-24 15:58:12 -0300

    Insert dataform in mysql

---
+++ exercicio-banco-dados/28-crud/php-action/create.php
@@ -0,0 +1,18 @@
+<?php
+//Conexão
+require_once 'db_connect.php';
+
+if(isset($_POST['btn-cadastrar'])):
+    $name = mysqli_escape_string($connect, $_POST['name']);
+    $lastname = mysqli_escape_string($connect, $_POST['lastname']);
+    $email = mysqli_escape_string($connect, $_POST['email']);
+    $age = mysqli_escape_string($connect, $_POST['age']);
+    
+    $sql = "INSERT INTO clientes (name, lastname, email, idade) VALUES ('$name', '$lastname','$email', '$age')";
+
+    if(mysqli_query($connect, $sql)):
+        header('Location: ../index.php ? sucesso');
+    else:
+        header('Location: ../index.php?erro');
+    endif;
+endif;
