d2ff63fd963a0387df72c704643ca069531f37a5
LilianePaixao <cpaixaoliliane@gmail.com>
2022-02-28 17:01:30 -0300

    Aulas 48 e 49

--- exercicio-banco-dados/28-crud/php-action/db_connect.php
+++ exercicio-banco-dados/28-crud/php-action/db_connect.php
@@ -1,11 +1,12 @@
 <?php
 // Conexão com banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "localhost:5555";
+$username = "acf";
+$password = "password";
 $db_name = "titas";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
+mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()):
     echo "Erro na conexão: " .mysqli_connect_error();
