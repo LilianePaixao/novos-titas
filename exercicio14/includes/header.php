8801a5ddbb5cf56b5abb38204e522fe822c771be
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-04 17:08:45 -0300

    Retirada da tag DOCTYPE e tag html.

--- exercicio14/includes/header.php
+++ exercicio14/includes/header.php
@@ -1,8 +1,11 @@
-<!DOCTYPE html>
-    <html>
+
         <head>
             <meta charset="utf-8">
             <title> Exercício 14</title>
+            
+            <link rel="icon" type="image/png" sizes="16x16"  href="/images/favicon-16x16.png">
+            <meta name="msapplication-TileColor" content="#ffffff">
+            <meta name="theme-color" content="#ffffff">
             <!--Import Google Icon Font-->
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
             <!--Import materialize.css-->
@@ -10,4 +13,4 @@
             <!--Let browser know website is optimized for mobile-->
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
-    </html>
+  
