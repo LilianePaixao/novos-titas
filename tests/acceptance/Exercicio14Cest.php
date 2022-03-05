6009b21a4b1317f09593804ed74afb044001111c
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-05 10:50:42 -0300

    Teste do exercicio14

--- tests/acceptance/Exercicio14Cest.php
+++ tests/acceptance/Exercicio14Cest.php
@@ -11,16 +11,16 @@ class Exercicios14Cest
         $I->fillField('numero', '110');
         $I->fillField('numero1', '150');
         $I->click("enviar");
-        $I->see('Serão necessários 41, para que Juca seja maior que Chico' );
+        $I->see('Serão necessários 41,' );
         //Numbers 150 and 110
         $I->fillField('numero', '150');
         $I->fillField('numero1', '110');
         $I->click("enviar");
-        $I->see('Serão necessários 0, para que Juca seja maior que Chico ' );
+        $I->see('Serão necessários 0, ' );
         //Numbers 100 and 110
         $I->fillField('numero', '100');
         $I->fillField('numero1', '110');
         $I->click("enviar");
-        $I->see('Serão necessários 11, para que Juca seja maior que Chico ' );
+        $I->see('Serão necessários 11, ' );
     }
 }
