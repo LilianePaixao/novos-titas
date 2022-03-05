8bb3c31c9a9efba0fb375ad81f2135f1c205df6e
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-05 16:02:34 -0300

    Correçao da conexao com banco de dados

--- exercicio14/php-action/db_connect.php
+++ exercicio14/php-action/db_connect.php
@@ -1,11 +1,11 @@
 <?php
 //Conexão com banco de dados
-$servername = "localhost:8282";
+$serverbancodedados = "localhost:5555";
 $username = "acf";
 $password = "password";
 $db_name = "exercicio14";
 
-$connect = mysqli_connect($servername, $username, $password, $db_name);
+$connect = mysqli_connect($serverbancodedados, $username, $password, $db_name);
 mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()){
