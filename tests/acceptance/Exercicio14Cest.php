bebcea5a9afff0da421b73191d5666e0217b00ef
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-05 16:23:06 -0300

    Logica e teste

--- tests/acceptance/Exercicio14Cest.php
+++ tests/acceptance/Exercicio14Cest.php
@@ -3,23 +3,23 @@
 class Exercicios14Cest
 {
     
-    public function Chico(AcceptanceTester $I)
+    public function studentsRegisterTests(AcceptanceTester $I)
     {
-        $I->amOnPage('/exercicio14a');
+        $I->amOnPage('exercicio14');
         $I->see('Exercício 14');
         //Numbers 110 and 150
-        $I->fillField('numero', '110');
-        $I->fillField('numero1', '150');
+        $I->fillField('numero1', '110');
+        $I->fillField('numero2', '150');
         $I->click("enviar");
         $I->see('Serão necessários 41,' );
         //Numbers 150 and 110
-        $I->fillField('numero', '150');
-        $I->fillField('numero1', '110');
+        $I->fillField('numero1', '150');
+        $I->fillField('numero2', '110');
         $I->click("enviar");
         $I->see('Serão necessários 0, ' );
         //Numbers 100 and 110
-        $I->fillField('numero', '100');
-        $I->fillField('numero1', '110');
+        $I->fillField('numero1', '100');
+        $I->fillField('numero2', '110');
         $I->click("enviar");
         $I->see('Serão necessários 11, ' );
     }
