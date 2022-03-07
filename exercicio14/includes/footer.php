dcbb96f14d82d0f8bffc871d20e988610c6dbbb4
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-07 11:54:49 -0300

    Inclusao de index.php message.php footer.php header.php adicionar.php

---
+++ exercicio14/includes/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+      <!--JavaScript at end of body for optimized loading-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    </footer>
+  </html>
