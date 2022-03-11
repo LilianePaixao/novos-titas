c1da80e663aedc210c4ffefc9cf147fadcf93d5b
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-11 13:24:27 -0300

    Correçao do teste referente ao exercicio 14.

--- tests/acceptance/Exercicio14Cest.php
+++ tests/acceptance/Exercicio14Cest.php
@@ -5,22 +5,35 @@ class Exercicios14Cest
     
     public function studentsRegisterTests(AcceptanceTester $I)
     {
-        $I->amOnPage('exercicio14');
+        $I->amOnPage('/exercicio14');
         $I->see('Exercício 14');
-        //Numbers 110 and 150
-        $I->fillField('numero1', '110');
-        $I->fillField('numero2', '150');
+        //Numbers 150 and 110
+        $I->fillField('higth1', '150');
+        $I->fillField('growth1','2');
+        $I->fillField('higth2', '110');
+        $I->fillField('growth2','3');
         $I->click("enviar");
         $I->see('Serão necessários 41,' );
-        //Numbers 150 and 110
-        $I->fillField('numero1', '150');
-        $I->fillField('numero2', '110');
+        //Numbers 110 and 150
+        $I->fillField('higth1', '110');
+        $I->fillField('growth1','2');
+        $I->fillField('higth2', '150');
+        $I->fillField('growth2','3');
         $I->click("enviar");
-        $I->see('Serão necessários 0, ' );
-        //Numbers 100 and 110
-        $I->fillField('numero1', '100');
-        $I->fillField('numero2', '110');
+        $I->see('Serão necessários 0' );
+        //Numbers 0 and 0
+        $I->fillField('higth1', '0');
+        $I->fillField('growth1','2');
+        $I->fillField('higth2', '0');
+        $I->fillField('growth2','3');
         $I->click("enviar");
-        $I->see('Serão necessários 11, ' );
+        $I->see('Infome os dados que se pedem. ' );
+        //Numbers 0 and 0
+        $I->fillField('higth1', '0');
+        $I->fillField('growth1','0');
+        $I->fillField('higth2', '0');
+        $I->fillField('growth2','0');
+        $I->click("enviar");
+        $I->see('Infome os dados que se pedem. ' );
     }
 }
