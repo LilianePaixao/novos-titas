e116df5e2cc69ec5de146c9c729e39ab17594ba0
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-11 13:48:35 -0300

    Deleçao da pasta do exercicio antigo 14.

--- exercicio14/includes/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer>
-      <!--JavaScript at end of body for optimized loading-->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    </footer>
-  </html>
