8a9d30153b03212a185fc929cf359b9363141483
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-18 17:20:30 -0300

    Conexao com banco de dados

--- exercicio15/index.php
+++ exercicio15/index.php
@@ -16,18 +16,22 @@ include_once 'db_connect.php';
 <body>
     <div>
         <h1> Exercício 15</h1>
-        <p> Na usina de Angra dos Reis, os técnicos analisam a perda de massa de um material
-radioativo.</p>
-        <form action = "exercicio15/index.php" method="POST">
+        <p> Na usina de Angra dos Reis, os técnicos analisam a perda de massa de um material radioativo.<br/>
+
+        Seu sistema deverá receber:<br/>
+        Um campo para informar a massa do material radioativo (em gramas)<br/>
+        Um campo para dizer a % de perda <br/> 
+        Um campo para dizer em quanto tempo perde essa % em segundos.</p>
+        <form action = "/exercicio15/index.php" method="POST">
             
             <label for="massa" > Digite a massa do material radioativo (g):</label>
-            <input type ="float" name ="massa" id="massa" size="20" placeholder="Exemplo 0.2">
+            <input type ="number" name ="massa" id="massa" size="20" placeholder="Exemplo 0.2">
 
             <label for="perda" > Porcentagem de perda:</label>
             <input type ="number" name ="perda" id="perda" size="20" placeholder="Use 25 para 25%">
 
-            <label for="tempoPerda" > Qual tempo para que essa perda ocorra(segundo)</label>
-            <input type ="number" name ="tempoPerda" id="tempoPerda" size="20" placeholder="segundos">
+            <label for="tempo_perda" > Qual tempo para que essa perda ocorra(segundo)</label>
+            <input type ="number" name ="tempo_perda"id="tempo_perda" size="20" placeholder="segundos">
             
             <button type ="submit" class="enviar">enviar</button>
 
@@ -37,12 +41,27 @@ radioativo.</p>
         //Trata os valores informados para evitar sql injection
         $massa = mysqli_escape_string($connect, $_POST['massa']);
         $perda = mysqli_escape_string($connect, $_POST['perda']);
-        $tempoPerda= mysqli_escape_string($connect, $_POST['tempoPerda']);
+        $tempoPerda= mysqli_escape_string($connect, $_POST['tempo_perda']);
        
-        $sql = "INSERT INTO Cientistas (massa,perda, tempoPerda) VALUES ('$massa', '$perda','$tempoPerda')";
-        
-            
-            function massRadioativ(){
+        $sql = "INSERT INTO Cientistas (massa, perda, tempo_perda) VALUES ('$massa', '$perda','$tempoPerda')";
+
+        try {
+          if ($mysqli = mysqli_query($connect, $sql)) {
+             // header('Location:index.php?sucesso');
+            exit;
+          } else {
+           //   header('Location:index.php?erro');
+           printf("Can't connect to localhost. Error: %s\n", mysqli_connect_error());
+           var_dump($connect);
+        }
+          
+          
+
+        } catch (Exception $e) {
+            echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+        } 
+
+            function massRadio(){
                 if (isset($_POST["massa"])){
                 
                     $tempoPerda = $_POST["tempoPerda"];
@@ -59,12 +78,12 @@ radioativo.</p>
                     }
                     $totalTime = $tempoPerda*$count;
 
-                    echo "Serão necessários " .  $totalTime/60 . " minutos, para que a massa do material seja menor que 0.10 gramas."; 
+                    echo "Serão necessários " .  $totalTime/60 . " minuto(s), para que a massa do material seja menor que 0.10 gramas."; 
                 } else {
                     echo "Informe os dados que se pedem.";
                 }
         }  
-        massRadioativ();
+        massRadio();
        ?>
     </div>
 </body>
