08653700e0dc5a7ad8d9450843a07dd483c85f63
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-25 17:11:28 -0300

    Alteraçoes realizadas de acordo com solicitaçoes no github.

---
+++ exercicio15/test_execute_connection.php
@@ -0,0 +1,12 @@
+<?php
+
+function test_execute_connection($connect, $sql)
+{
+    try {
+        if (!$mysqli = mysqli_query($connect, $sql)) {
+            printf("Houve problema na conexão. Tente novamente mais tarde. Error: %s\n", mysqli_connect_error());
+        }
+    } catch (Exception $e) {
+        echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+    } 
+}        
