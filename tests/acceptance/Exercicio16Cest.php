708139dd2e1401c98e07a9036206bd5d1b7ce541
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-28 14:00:02 -0300

    Mudança nos comentarios e inclusao de testes impares e input vazio.

--- tests/acceptance/Exercicio16Cest.php
+++ tests/acceptance/Exercicio16Cest.php
@@ -7,9 +7,14 @@ class Exercicios16Cest
     {
         $I->amOnPage('/exercicio16');
         $I->see('Exercício 16');
-        //Numbers 150 and 110
+        // positive numbers
         $I->fillField('numerosDigitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15');
         $I->click("enviar");
         $I->see('Os números pares são Array ( [0] => 2 [1] => 4 [2] => 6 ' );
+        $I->see('Os números ímpares são Array ( [0] => 1 [1] => 3 [2] => 5 ' );
+        // negative numbers
+        $I->fillField('numerosDigitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15');
+        $I->click("enviar");
+        $I->see('Os números informados devem ser positivos' );
     }
 }
