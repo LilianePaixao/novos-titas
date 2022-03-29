662e5be841e2765cf4e41a3eae961d4d8a92c5d2
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-29 15:49:24 -0300

    Refatoraçao do codigo do teste.

--- tests/acceptance/Exercicio17Cest.php
+++ tests/acceptance/Exercicio17Cest.php
@@ -7,9 +7,18 @@ class Exercicios17Cest
     {
         $I->amOnPage('/exercicio17');
         $I->see('Exercício 17');
+        //positive numbers
         $I->fillField('numerosDigitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20    
         ');
         $I->click("enviar");
-        $I->see('O maior número é 20A média será210 ' );
+        $I->see('O maior número informado é 20. A média será 210. A porcentagem de números pares é 50 %.' );
+        //negative numbers
+        $I->fillField('numerosDigitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15,16,17,18,19,20');
+        $I->click("enviar");
+        $I->see('Os números informados devem ser positivos.' );
+        //zero number
+        $I->fillField('numerosDigitados', '0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20');
+        $I->click("enviar");
+        $I->see('Os números informados devem ser positivos.' );
     }
 }
