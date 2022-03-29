40202ff61ef9286c4f6f658cce3b2c1e3c89af72
LilianePaixao <cpaixaoliliane@gmail.com>
2022-03-29 16:28:15 -0300

    Inclusao de media corrigida e menor numero.

--- tests/acceptance/Exercicio17Cest.php
+++ tests/acceptance/Exercicio17Cest.php
@@ -11,7 +11,7 @@ class Exercicios17Cest
         $I->fillField('numerosDigitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20    
         ');
         $I->click("enviar");
-        $I->see('O maior número informado é 20. A média será 210. A porcentagem de números pares é 50 %.' );
+        $I->see('O menor número informado é 1. O maior número informado é 20. A média será 10.5. A porcentagem de números pares é 50 %.' );
         //negative numbers
         $I->fillField('numerosDigitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15,16,17,18,19,20');
         $I->click("enviar");
