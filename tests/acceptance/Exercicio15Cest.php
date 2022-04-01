6436ea42e09d1caa97e5ed7a0dcef1e526babe5d
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-01 17:22:08 -0300

    Fix look for see in DB.

--- tests/acceptance/Exercicio15Cest.php
+++ tests/acceptance/Exercicio15Cest.php
@@ -10,19 +10,19 @@ class Exercicios15Cest
         //Numbers 0.10 and 25%
         $I->fillField('massa', '0.10');
         $I->fillField('perda', '25');
-        $I->fillField('tempo_perda, 40');
+        $I->fillField('tempo_perda', '40');
         $I->click("enviar");
-        $I->see('Serão necessários 0.66666666666667 minuto(s), para que a massa do material seja menor que 0.10 gramas. ' );
+        $I->see('Serão necessários 0.66666666666667 minuto(s), para que a massa do material seja menor que 0.10 gramas.' );
         //Numbers 100 and 50%
         $I->fillField('massa', '100');
         $I->fillField('perda', '50');
-        $I->fillField('tempoPerda, 60');
+        $I->fillField('tempoPerda', '60');
         $I->click("enviar");
         $I->see('Serão necessários 10 minuto(s), para que a massa do material seja menor que 0.10 gramas. ' );
         //Numbers -10 and 40% and time 60
         $I->fillField('massa', '-10');
         $I->fillField('perda', '40');
-        $I->fillField('tempoPerda, 60');
+        $I->fillField('tempoPerda', '60');
         $I->click("enviar");
         $I->see('Serão necessários 0 minuto(s), para que a massa do material seja menor que 0.10 gramas. ' );
         #Database test
