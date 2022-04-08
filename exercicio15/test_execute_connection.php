1ecf68a10ab07919e9e0f291cf164525df4cc97f
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-08 15:46:16 -0300

    Adjust in tempo_perda and databases tests.

--- exercicio15/test_execute_connection.php
+++ exercicio15/test_execute_connection.php
@@ -1,10 +1,12 @@
 <?php
+//Conexão
+include_once 'db_connect.php';    
 
 function test_execute_connection($connect, $sql)
 {
     try {
         if (!$mysqli = mysqli_query($connect, $sql)) {
-            printf("Houve problema na conexão. Tente novamente mais tarde. Error: %s\n", mysqli_connect_error());
+            //printf("Houve problema na conexão. Tente novamente mais tarde. Error: %s\n", mysqli_connect_error());
         }
     } catch (Exception $e) {
         //echo 'Exceção capturada: ',  $e->getMessage(), "\n";
