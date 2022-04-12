7e268f657b18a6d93ce58bd26fb319e404f19b01
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-12 12:41:36 -0300

    Fix the test included the datas of databases.

--- tests/acceptance/Exercicio16Cest.php
+++ tests/acceptance/Exercicio16Cest.php
@@ -7,16 +7,14 @@ class Exercicios16Cest
     {
         $I->amOnPage('/exercicio16');
         $I->see('Exercício 16');
-        // positive numbers
-        $I->fillField('numerosDigitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15');
-        $I->click("enviar");
-        $I->see('Os números pares são Array ( [0] => 2 [1] => 4 [2] => 6 ' );
-        $I->see('Os números ímpares são Array ( [0] => 1 [1] => 3 [2] => 5 ' );
         // negative numbers
-        $I->fillField('numerosDigitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15');
+        $I->fillField('numeros_digitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15');
         $I->click("enviar");
         $I->see('Os números informados devem ser positivos' );
-        #Database test
-        $I->seeInDatabase('Numeros', ['id' => '40', 'numeros_digitados' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15', 'numeros_pares'=>'2,4,6,8,10,12,14', 'numeros_impares' => '1,3,5,7,9,11,13,15']);
+        // positive numbers
+        $I->fillField('numeros_digitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15');
+        $I->click("enviar");
+        // Database test
+        $I->seeInDatabase('exercicio16', ['id' => '23', 'numeros_digitados' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15', 'numeros_pares'=>'2,4,6,8,10,12,14', 'numeros_impares' => '1,3,5,7,9,11,13,15']);
     }
 }
