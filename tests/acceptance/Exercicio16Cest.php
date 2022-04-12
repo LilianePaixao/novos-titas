969c931972ba99497e01797be891ebd9a1cb29e0
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-12 16:07:04 -0300

    Fix to root

--- tests/acceptance/Exercicio16Cest.php
+++ tests/acceptance/Exercicio16Cest.php
@@ -15,6 +15,6 @@ class Exercicios16Cest
         $I->fillField('numeros_digitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15');
         $I->click("enviar");
         // Database test
-        $I->seeInDatabase('exercicio16', ['id' => '23', 'numeros_digitados' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15', 'numeros_pares'=>'2,4,6,8,10,12,14', 'numeros_impares' => '1,3,5,7,9,11,13,15']);
+        $I->seeInDatabase('exercicio16', ['id' => '1', 'numeros_digitados' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15', 'numeros_pares'=>'2,4,6,8,10,12,14', 'numeros_impares' => '1,3,5,7,9,11,13,15']);
     }
 }
