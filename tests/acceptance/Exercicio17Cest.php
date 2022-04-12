0e42c90e04674d35740541d87b9306103226bea2
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-12 17:58:42 -0300

    Test correct

--- tests/acceptance/Exercicio17Cest.php
+++ tests/acceptance/Exercicio17Cest.php
@@ -8,19 +8,18 @@ class Exercicios17Cest
         $I->amOnPage('/exercicio17');
         $I->see('Exercício 17');
         //positive numbers
-        $I->fillField('numerosDigitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20    
+        $I->fillField('numeros_digitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20    
         ');
         $I->click("enviar");
-        $I->see('O menor número informado é 1. O maior número informado é 20. A média será 10.5. A porcentagem de números pares é 50 %.' );
+        $I->seeInDatabase('exercicio17', ['numeros_digitados' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20', 'menorNumero' => '1', 'maiorNumero'=>'20', 'media'=> '10.5', 'porcentagem'=>'50' ]);
+        // with zero number
+        $I->fillField('numeros_digitados', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20    
+        ');
+        $I->click("enviar");
+        $I->seeInDatabase('exercicio17', ['numeros_digitados' => '0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20', 'menorNumero' => '0', 'maiorNumero'=>'20', 'media'=> '10.45', 'porcentagem'=>'55' ]);
         //negative numbers
-        $I->fillField('numerosDigitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15,16,17,18,19,20');
+        $I->fillField('numeros_digitados', '-1,-2,-3,-4,-5,-6,-7,-8,-9,10,11,12,13,14,15,16,17,18,19,20');
         $I->click("enviar");
         $I->see('Os números informados devem ser positivos.' );
-        //zero number
-        $I->fillField('numerosDigitados', '0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20');
-        $I->click("enviar");
-        $I->see('Os números informados devem ser positivos.' );
-        //BD query
-        $I->seeInDatabase('Numeros', ['numerosDigitados' => '0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20', 'menorNumero' => 'O menor número informado é 1.', 'maiorNumero'=>'O maior número informado é 20.', 'media'=> 'A média será 10.5.', 'porcentagem'=>'A porcentagem de números pares é 50 %.' ]);
     }
 }
