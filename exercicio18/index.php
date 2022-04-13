3bc94fe1aa8910b7171cb52247e06db56489b61a
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-13 14:44:28 -0300

    Programing to exercise 18.

--- exercicio18/index.php
+++ exercicio18/index.php
@@ -8,8 +8,8 @@ include_once './includes/header.php';
 
 <body>
   <div class = "container" style="width:700px" id="centro">
-      <h3 class="ligth">Dados Informados</h3>
-        <form action = "/exercicio16/index.php" method="POST">
+      <h3 class="ligth">Exercício 18</h3>
+        <form action = "/exercicio18/index.php" method="POST">
             <label for="numero"> Informe 10 números separados com ",":</label>
             <input type ="text" size= "100" maxlength ="50" name = "numbers1" placeholder = "1,2,110,200,300,0...">
             <label for="numero"> Informe 10 números separados com ",":</label>
@@ -30,50 +30,43 @@ include_once './includes/header.php';
             $stringNumbers1 = implode(",", $arrayNumbers1);
             $stringNumbers2 = implode(",", $arrayNumbers2);
             $stringresult = implode(",", $result);
-
+            
             //Treatment to avoid sql injection
             $stringNumbers1 = mysqli_escape_string($connect, $stringNumbers1);
             $stringNumbers2 = mysqli_escape_string($connect, $stringNumbers2 );
             $stringresult= mysqli_escape_string($connect, $stringresult);
-
-            $sql = "INSERT INTO exercicio17 (`numbers1`, `numbers2`, `result`) VALUES ('$stringNumbers1', '$stringNumbers2','$stringresult')";  
-
+            
+            $sql = "INSERT INTO exercicio18 (`numbers1`, `numbers2`, `result`) VALUES ('$stringNumbers1', '$stringNumbers2','$stringresult')";  
+            
             execute_connection($connect, $sql);
-            }
-            else {
-                echo "Preencha os campos conforme solicitado.";
+            
+            //Print datas
+            $sql = "SELECT * FROM exercicio18";
+            $resultado = execute_connection($connect, $sql);
+            
+            ?>
+                <table>
+                <tr>
+                    <th> id </th>  
+                    <th> Primeira lista de números informados </th>
+                    <th> Segunda lista de números informados </th> 
+                    <th> Números que existem somente na primeira lista e não na segunda</th>  
+                </tr>
+            <?php
+                
+                while ($dados = mysqli_fetch_array($resultado)){
+            ?>
+                <tr>
+                    <td><?= $dados['id'];?> </td>
+                    <td><?= $dados['numbers1'];?> </td>
+                    <td><?= $dados['numbers2'];?></td>
+                    <td><?= $dados['result'];?></td>
+                </tr>
+                </table>  
+            <?php
+                }    
+
         }   
-        
-        
-
-        //Print datas
-        $sql = "SELECT * FROM exercicio16";
-        $resultado = execute_connection($connect, $sql);
-
-    ?>
-
-<?php
-    
-    while ($dados = mysqli_fetch_array($resultado)){
-        ?>
-        <table>
-        <tr>
-            <th> id </th>  
-            <th> Números digitados </th>
-            <th> Números pares </th> 
-            <th> Números ímpares </th>  
-        </tr>
-        <tr>
-            <td><?= $dados['id'];?> </td>
-            <td><?= $dados['numeros_digitados'];?> </td>
-            <td><?= $dados['numeros_pares'];?></td>
-            <td><?= $dados['numeros_impares'];?></td>
-        </tr>
-        </table>  
-    <?php
-    }        
-    ?>
-<?php
 //footer
 include_once './includes/footer.php';
 ?>
