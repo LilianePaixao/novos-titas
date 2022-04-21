98e534ba9770e8c4dd26dfa2bccf4b69b9c190c3
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-21 10:59:52 -0300

    Inclusão de use exercicios.

---
+++ exercicio18/exercicios18.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: dbMysql
+-- Tempo de geração: 13/04/2022 às 17:41
+-- Versão do servidor: 8.0.28
+-- Versão do PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+USE exercicios;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `exercicios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `exercicio18`
+--
+
+CREATE TABLE `exercicio18` (
+  `id` int NOT NULL,
+  `numbers1` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `numbers2` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `result` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `exercicio18`
+--
+ALTER TABLE `exercicio18`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `exercicio18`
+--
+ALTER TABLE `exercicio18`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
