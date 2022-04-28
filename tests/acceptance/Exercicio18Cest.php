9ad3512662de46dd5056a448ef26669b413f95a1
LilianePaixao <cpaixaoliliane@gmail.com>
2022-04-28 11:03:05 -0300

    Fix the test

--- tests/acceptance/Exercicio18Cest.php
+++ tests/acceptance/Exercicio18Cest.php
@@ -8,9 +8,9 @@ class Exercicios18Cest
         $I->amOnPage('/exercicio18');
         $I->see('Exercício 18');
         //first fill
-        $I->fillField('numbers1', '1,2,3,4,5,6,7,8,9,0');
-        $I->fillField('numbers2', '-1,-2,-3,-5,-7,-9,10,11,15,16');
-        $I->click("enviar");
-        $I->seeInDatabase('exercicio18', ['numbers1' => '1,2,3,4,5,6,7,8,9,0', 'numbers2'=>'-1,-2,-3,-5,-7,-9,10,11,15,16', 'result' => '1,2,3,4,5,6,7,8,9,0,-1,-2,-3,-5,-7,-9,10,11,15,16']);
+        $I->fillField('numbers1','1,2');
+        $I->fillField('numbers2','-1,1,2');
+        $I->click('enviar');
+        $I->seeInDatabase('exercicio18', ['numbers1' =>'1,2', 'numbers2'=>'-1,1,2', 'result' => ' ,-1']);
     }
 }
